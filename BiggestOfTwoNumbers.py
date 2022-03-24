3c24e83b6818bb9653f17e17f1843bec3b5148dd
khushi173 <khushijamwal173@gmail.com>
2022-03-24 17:36:51 +0530

    updated

--- BiggestOfTwoNumbers.py
+++ BiggestOfTwoNumbers.py
@@ -2,20 +2,20 @@ import unittest
 
 def BiggestOfTwoNumbers(x,y):
     if x>y:
-        return x
+        return "number is bigger"
     else:
-        return y
+        return "number is smaller"
 class  BiggestNumbers(unittest.TestCase):
     def test_case1(self):
         x = 40
         y = 30
         result = BiggestOfTwoNumbers(x,y)
-        self.assertEqual(x,result)
+        self.assertEqual("number is bigger",result)
     def test_case2(self):
         x=60
         y=88
         result = BiggestOfTwoNumbers(x,y)
-        self.assertEqual(y,result)
+        self.assertEqual("number is smaller",result)
 
 if  __name__=="__main__":
     unittest.main()
